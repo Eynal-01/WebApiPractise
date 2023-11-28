1dd7ccd2ddcf861f56dc51160c77862abeb969ac
Eynal <Eynal@DESKTOP-I662UI3>
2023-11-28 12:06:54 +0400

    Integrated Csv input and output formatter classes

---
+++ WebApiTask1/Formatters/CsvInputFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace WebApiTask1.Formatters
+{
+    public class CsvInputFormatter
+    {
+        public VcardInputFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/vcard"));
+
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+    }
+}
